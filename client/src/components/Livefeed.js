4ed98badf0c65f6c914d82c957bee32da60b3258
Jam.zD <jzdavis1212@gmail.com>
2021-01-06 22:06:35 +0000

    edit height of livefeed component

--- client/src/components/Livefeed.js
+++ client/src/components/Livefeed.js
@@ -3,7 +3,7 @@ import './Livefeed.css';
 
 const Livefeed = () => {
     return (
-        <div className="container-fluid">
+        <div id="livefeed" className="container-fluid">
             <div className="row">
                 <div className="col">
                     <div className="tagline">
