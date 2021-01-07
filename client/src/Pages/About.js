37a02db13fe3c5483eb29a47274aabdb6278e4ae
Jam.zD <jzdavis1212@gmail.com>
2021-01-07 16:32:45 +0000

    update file path for images

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,9 +1,9 @@
 import React from 'react'; 
 import './About.css';  
-import MMLogo from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/MMLogo.png';
-import med from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/med.jpg'; 
-import woman2 from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/woman2.jpg'; 
-import woman3 from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/woman3.png'; 
+import MMLogo from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/MMLogo.png';
+import med from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/med.jpg'; 
+import woman2 from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/woman2.jpg'; 
+import woman3 from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/woman3.png'; 
  
 
 const About = () => {
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 import './Footer.css'; 
-import guard from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/guard.png'; 
-import hw from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/hw.png'; 
-import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/images/psych.png'; 
+import guard from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/guard.png'; 
+import hw from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/hw.png'; 
+import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/psych.png'; 
 
 
 const Footer = () => {
