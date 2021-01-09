e60a07fe01f7a6ccd5a1bfbacb0d2a9865f3c797
Jam.zD <jzdavis1212@gmail.com>
2021-01-09 19:11:42 +0000

    add router to create link for provacy policy page and style footer by adding logo image and headings

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
+import { Link } from 'react-router-dom'; 
 import './Footer.css'; 
+import logo from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/logo.png'; 
 import guard from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/guard.png'; 
 import hw from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/hw.png'; 
 import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/psych.png'; 
@@ -8,29 +10,24 @@ import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/psych.png
 const Footer = () => {
   return (
     <footer className="container-fluid">
-      
-        <div className="col-sm" id="trustLinks">
-          <p>As Featured in:</p>
-          <img id="guard" className="img-fluid" src={guard} alt="the guardian"></img>
-          <img id="hw" className="img-fluid" src={hw} alt="health and wellbeing"></img>
-          <img id="psych" className="img-fluid" src={psych} alt="psychology today"></img> 
-        </div>
-        <div id="bottomFooter">
-          <p className="col-sm">©2021 All rights reserved.</p>     
-        </div>
-      
-    </footer>
+      <div id="footer" className="col-sm">
+        <h1 id="footHead1">
+          <img id="footImg" src={logo} alt="Logo"/>
+          Mindful Matters
+        </h1>
+        <h2 id="footHead">AS FEATURED IN:</h2>
+        <img id="guard" className="img-fluid" src={guard} alt="the guardian"></img>
+        <img id="hw" className="img-fluid" src={hw} alt="health and wellbeing"></img>
+        <img id="psych" className="img-fluid" src={psych} alt="psychology today"></img>
+        <hr/>
+        <nav>
+          <Link className="nav-link" id="navPrivacy" to="/Privacy">Privacy</Link>
+        </nav>
+        <p id="copywrite">©2021 All rights reserved.</p>     
+      </div> 
+   </footer>
   );   
 }
 
 export default Footer; 
 
-/* 
-  <div className="col">
-    <h3>Find out more</h3>
-    <nav></nav> 
-  <div className="col">
-    <p>©2021 All rights reserved.</p>     
-  </div> 
-
-*/ 
