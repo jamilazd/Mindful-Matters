8b8949b0be3212f57372dcc30830f302dcb56872
Jam.zD <jzdavis1212@gmail.com>
2021-01-10 16:18:06 +0000

    create <h1>, <p> and add background colour, move embedded video player and experiment with adding cards

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css'; 
+import { Link } from 'react-router-dom'; 
 import ReactPlayer from "react-player"; 
+import MMLogo from '/Users/jd/Desktop/Mindful-Matters/client/src/assets/MMLogo.png';
 
 
 const Home = () => {
@@ -15,11 +17,30 @@ const Home = () => {
                             <i id="italic"> Mindful Matters' mission is to encourage steps towards better mental wellbeing.</i>
                         </u>
                     </div>
+                    <div id="homeMessage">
+                       <h2 className="homeHead">Be Kind To Yourself</h2>
+                       <p id="homeP">
+                           Mindfulness. It all starts with just a few minutes a day.
+                           Reduce stress and build mental resilience.  
+                           <img className="img-fluid" id="MMLogo4" src={MMLogo} alt="Brand Logo" />
+                       </p>
+                       <hr />
+                       <nav>
+                            <Link id="homeLink" className="nav-link" to="/Video">Start Your Journey</Link>
+                        </nav>
+                    </div>
                     <div id="vidContainer" className="col">
                        <div class="embed-responsive embed-responsive" id="videoPlayer">
                           <ReactPlayer url="https://www.youtube.com/watch?v=APX33ncDER0"/>  
                        </div>   
                   </div>  
+                  <div id="homeStories">
+                      <h2 className="homeHead">
+                          Mindful Matters Stories 
+                      </h2>
+                  </div>
+                  
+                       
                </div>
             </div>
         </div>
@@ -30,7 +51,20 @@ const Home = () => {
 export default Home; 
 
 /*  
+<div className="container">
 
+<div className="card" >
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div> 
+                  </div>   
+
+
+
+
+</div>
  
 
 
