ddd544892767ee09e8c81b625d5964bdc8540743
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2021-01-10 21:38:58 +0000

    Swaps out guard for the guard asset link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,36 +1,36 @@
-import React from 'react'; 
-import './Footer.css'; 
-import guard from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/guard.png'; 
-import hw from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/hw.png'; 
-import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/psych.png'; 
+import React from 'react';
+import './Footer.css';
+// we shouldn't import the links
+import hw from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/hw.png';
+import psych from '/Users/jd/Desktop/Mindful-Matters/client/src/Assets/psych.png';
 
 
 const Footer = () => {
   return (
     <footer className="container-fluid">
-      
+
         <div className="col-sm" id="trustLinks">
           <p>As Featured in:</p>
-          <img id="guard" className="img-fluid" src={guard} alt="the guardian"></img>
+          <img id="guard" className="img-fluid" src="http://localhost:5000/static/guard.png" alt="the guardian"></img>
           <img id="hw" className="img-fluid" src={hw} alt="health and wellbeing"></img>
-          <img id="psych" className="img-fluid" src={psych} alt="psychology today"></img> 
+          <img id="psych" className="img-fluid" src={psych} alt="psychology today"></img>
         </div>
         <div id="bottomFooter">
-          <p className="col-sm">©2021 All rights reserved.</p>     
+          <p className="col-sm">©2021 All rights reserved.</p>
         </div>
-      
+
     </footer>
-  );   
+  );
 }
 
-export default Footer; 
+export default Footer;
 
-/* 
+/*
   <div className="col">
     <h3>Find out more</h3>
-    <nav></nav> 
+    <nav></nav>
   <div className="col">
-    <p>©2021 All rights reserved.</p>     
-  </div> 
+    <p>©2021 All rights reserved.</p>
+  </div>
 
-*/ 
+*/
