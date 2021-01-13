d489d1ab640343cca2144afe1e91d6484920c519
Jam.zD <jzdavis1212@gmail.com>
2021-01-13 17:46:08 +0000

    create component for tagline card and import into About and Home pages

---
+++ client/src/components/TaglineCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TaglineCard.css'; 
+
+
+const TaglineCard = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+            <div className="col">
+                <div className="tagline">
+                    <h2 id="h2">Your Mind Matters </h2>
+                    <br/>
+                    <u>
+                        <i id="italic"> Mindful Matters' mission is to encourage steps towards better mental wellbeing.</i>
+                    </u>
+                    </div>
+               </div>
+            </div>     
+       </div>
+    ); 
+}
+
+export default TaglineCard; 
