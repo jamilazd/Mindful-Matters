6b44c2b575eb03fa0756f243b79d83581a3f3605
Jam.zD <jzdavis1212@gmail.com>
2021-01-20 17:37:32 +0000

    change heading outline

--- client/src/components/TaglineCard.js
+++ client/src/components/TaglineCard.js
@@ -8,7 +8,7 @@ const TaglineCard = () => {
             <div className="row">
             <div className="col">
                 <div className="tagline">
-                    <h2 id="h2">Your Mind Matters </h2>
+                    <i><h2 id="h2">Your Mind Matters </h2></i>
                     <br/>
                     <u>
                         <i id="italic"> Mindful Matters' mission is to encourage steps towards better mental wellbeing.</i>
