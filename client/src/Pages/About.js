64bbb5213ca3b8434e7495a8a896b38ec72a3da3
Jam.zD <jzdavis1212@gmail.com>
2021-01-22 22:40:23 +0000

    adjust margins for header and paragraph

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -6,13 +6,12 @@ const About = () => {
     return (
         <div className="container-fluid">
             <div id="explain" className="col">
-               <h2 id="aboutHead">About</h2>
+               <h1 id="aboutHead">About</h1>
                <p id="aboutP">
                   Mindful Matters is on a mission to encourage steps towards better mental wellbeing, by 
                   promoting Mindfulness Meditation. Mindfullness is the process of acknowledging your feelings 
                   in the moment, without interruption or judgement. You can use Mindful Matters to learn the 
                   basics of Mindfulness and catch up on the latest mental wellbeing news. 
-                  <img className="img-fluid" id="MMLogo" src="http://localhost:5000/static/MMLogo.png" alt="Brand Logo" />
                </p> 
             </div>
             <div className="col" id="photos">
