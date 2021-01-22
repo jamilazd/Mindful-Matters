b665bb221aaf279be30db4ae5bb9944beaeed6cd
Jam.zD <jzdavis1212@gmail.com>
2021-01-22 22:43:23 +0000

    adjust nav hover colours

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,23 +5,25 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header id="head" className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <ul id="nav" className="nav nav-tabs nav justify-content-end navbar navbar-expand-lg navbar-light"> 
-            <nav id="navBrand" className="navbar-brand">
-              Mindful Matters
-            </nav>
-            <nav className="nav-item">
-              <Link id="navStyle" className="nav-link Home" to="/">Home</Link>
-            </nav>
-            <nav className="nav-item">
-              <Link id="navStyle"  className="nav-link Video" to="/Video">Videos</Link>
-            </nav>
-            <nav className="nav-item">
-              <Link id="navStyle" className="nav-link About" to="/About">About</Link>
-            </nav>
-          </ul>
+    <header>
+      <div className="container-fluid">
+         <div className="row">
+          <div className="col">
+            <ul id="nav" className="nav nav-tabs nav justify-content-end navbar navbar-expand-lg navbar-light"> 
+              <nav id="navBrand" className="navbar-brand">
+                Mindful Matters
+              </nav>
+              <nav className="nav-item">
+                <Link id="navStyle" className="nav-link Home" to="/">Home</Link>
+              </nav>
+              <nav className="nav-item">
+                <Link id="navStyle"  className="nav-link VideoPage" to="/VideoPage">Videos</Link>
+              </nav>
+              <nav className="nav-item">
+                <Link id="navStyle" className="nav-link About" to="/About">About</Link>
+              </nav>
+            </ul>
+          </div>  
         </div>
       </div>
     </header> 
