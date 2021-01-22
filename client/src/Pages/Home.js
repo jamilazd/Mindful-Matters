9c939531a563a70468dfc6d4e1ce55863230e9ec
Jam.zD <jzdavis1212@gmail.com>
2021-01-22 22:45:10 +0000

    chnage heading colours, adjust element padding and margins

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -10,30 +10,26 @@ const Home = () => {
         <div className="container-fluid" id="contain">
             <div className="row">
                 <div className="col">
-                    <div id="homeMessage">
-                       <h2 id="emailHead">Recieve News Directly to your Inbox</h2>
-                        <form id="emailBox" >
-                            <input  id="boxSize" type="text" name="email" placeholder="Email address" />
-                            <input id="button" type="submit" className="btn btn-primary" value="Submit" />
-                       </form>
-                       <hr />
-                       <h2 className="homeHead">Be Kind To Yourself</h2>
+                    <div id="pBox">
+                       <h1 id="headHome">Be Kind to Yourself</h1>
                        <p id="homeP">
                            It all starts with just a few minutes a day to begin reducing stress and building
                            mental resilience. Mindfulness really is for everyone! You can try one of our mindfulness videos 
-                           and start learning how to incorporate Mindfulness practice into your life. 
-                           <img className="img-fluid" id="MMLogo2" src="http://localhost:5000/static/MMLogo.png" alt="Brand Logo" />
+                           and start learning how to incorporate mindfulness practice into your life. 
+                           <nav  id="navLink">
+                              <Link id="Link"  to="/VideoPage">Start Your Journey with Mindful Matters</Link>
+                           </nav>
                        </p>
-                       <nav>
-                            <Link id="homeLink"  to="/Video">Start Your Journey</Link>
-                        </nav>
+                       <hr className="hrStyle"/>
                     </div>
                     <div id="vidContainer">
+                        <h2 id="headVid">What is Mindfulness?</h2>
                        <div className="embed-responsive embed-responsive" id="videoPlayer">
                           <ReactPlayer url="https://www.youtube.com/watch?v=APX33ncDER0"/>  
                        </div>   
                   </div>  
                   <div>
+                      <hr className="hrStyle"/>
                       <h2 id="homeStories">
                           Mindful Matters Stories 
                       </h2>
