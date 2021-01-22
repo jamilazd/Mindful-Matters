4b06707568a1741edd535dba87b406f8a5205d16
Jam.zD <jzdavis1212@gmail.com>
2021-01-22 22:41:57 +0000

    restyle footer by resizing images and change heading colours

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,16 +11,16 @@ const Footer = () => {
         <h1 id="footHead1">
           <img id="footImg" src="http://localhost:5000/static/logo.png" alt="Logo"/>
           Mindful Matters
-        </h1>
-        <hr />
-        <h2 id="footHead">AS FEATURED IN:</h2>
-        <img id="guard" className="img-fluid" src="http://localhost:5000/static/guard.png" alt="the guardian"></img>
-        <img id="hw" className="img-fluid" src="http://localhost:5000/static/hw.png" alt="health and wellbeing"></img>
-        <img id="psych" className="img-fluid" src="http://localhost:5000/static/hw.psych" alt="psychology today"></img>
+        </h1>  
         <nav>
           <Link className="nav-link" id="navPrivacy" to="/Privacy">Privacy</Link>
         </nav>
-        <p id="copywrite">©2021 All rights reserved.</p>     
+        <h2 id="footHead">AS FEATURED IN:</h2>
+        <img id="hw" className="img-fluid" src="http://localhost:5000/static/hw.png" alt="health and wellbeing"></img>
+        <img id="psych" className="img-fluid" src="http://localhost:5000/static/psych.png" alt="psychology today"></img>
+        <img id="guard" className="img-fluid" src="http://localhost:5000/static/guard.png" alt="the guardian"></img> 
+        <hr className="hr"/>
+        <p id="copyright">Copyright © Mindful Matters 2020</p>
       </div> 
    </footer>
   );   
