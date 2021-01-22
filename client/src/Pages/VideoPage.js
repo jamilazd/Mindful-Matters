0572daaca53406df897d003c2f3eb35350d85aba
Jam.zD <jzdavis1212@gmail.com>
2021-01-22 22:39:34 +0000

    create videopage component and style video component and image thumbnails

---
+++ client/src/Pages/VideoPage.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import './VideoPage.css'; 
+
+const VideoPage = () => {
+  return (
+    <div id="textContainer" className="container-fluid">
+      <div id="textBackground" className="col">
+        <h1 id="videoHead">Stop for a moment...</h1>
+        <p id="videoP">
+          Mindfulness meditation can help reduce stress and relax the mind.
+          Below is a carefully curated selection of mindfulness videos you can watch now. 
+        </p>
+      </div> 
+   </div>
+  ); 
+}
+
+
+
+export default VideoPage; 
---
+++ client/src/components/Video.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import './Video.css'; 
+
+const Video = (props) => {
+
+  const { thumbnail, videoLink, videoName, description } = props.video; 
+  return(
+    <div className="container-fluid">
+      <div className="row" >
+        <div className="col">
+          <img id="vidThumb" className="img-fluid" src={thumbnail} alt="thumbnailImage" />
+          <div id="vidDescription">
+            <a href={videoLink} target="blank"> <h1 id="vidName">{videoName}</h1> </a>
+            <p id="description">{description}</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  ); 
+}
+
+export default Video;
+
+
+
+
