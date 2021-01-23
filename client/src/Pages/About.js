e1b3afc78af63a3b7e0ca16e81eee4937a460742
Jam.zD <jzdavis1212@gmail.com>
2021-01-23 13:27:16 +0000

    style videopage padding

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -13,11 +13,7 @@ const About = () => {
                   in the moment, without interruption or judgement. You can use Mindful Matters to learn the 
                   basics of Mindfulness and catch up on the latest mental wellbeing news. 
                </p> 
-            </div>
-            <div className="col" id="photos">
-              <img className="img-fluid" id="woman1" src="http://localhost:5000/static/med.jpg" alt="meditating woman" />
-              <img className="img-fluid" id="woman2" src="http://localhost:5000/static/woman2.jpg" alt="meditating woman" />
-              <img className="img-fluid" id="woman3" src="http://localhost:5000/static/woman3.jpg" alt="meditating woman" />
+               <hr className="hrStyle"/>
             </div>
         </div>
     );
