2a77fb460aad89c734e51ba766ade7605b159e13
Jam.zD <jzdavis1212@gmail.com>
2021-02-02 17:24:33 +0000

    resize p and p margins

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -1,17 +1,18 @@
 import React from 'react'; 
+import { Link } from 'react-router-dom'; 
 import './About.css';  
- 
 
 const About = () => {
     return (
-        <div className="container-fluid">
+        <div >
             <div id="explain" className="col">
                <h1 id="aboutHead">About Us</h1>
                <p id="aboutP">
                   Mindful Matters is on a mission to encourage steps towards better mental wellbeing, by 
                   promoting mindfulness meditation. Mindfullness is the process of acknowledging your feelings 
                   in the moment, without interruption or judgement. You can use Mindful Matters to learn the 
-                  basics of Mindfulness and catch up on the latest mental wellbeing news. 
+                  basics of mindfulness practice, watch beginner friendly mindfulness meditation videos 
+                  and catch up on the latest mental wellbeing news from across the UK. 
                </p> 
             </div>
         </div>
@@ -21,6 +22,12 @@ const About = () => {
 export default About; 
 
 
+/* 
+   <nav>
+      <Link className="nav-link" id="navPrivacy" to="/Privacy">Privacy</Link>
+   </nav>
+
+*/
 
 
 
