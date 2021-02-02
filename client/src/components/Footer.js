052dbd5086b82f69f4835d942310ec112fe5642d
Jam.zD <jzdavis1212@gmail.com>
2021-02-02 17:25:05 +0000

    change footer background and remove nav link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,20 +1,16 @@
 import React from 'react'; 
-import { Link } from 'react-router-dom'; 
 import './Footer.css'; 
 
 
 
 const Footer = () => {
   return (
-    <footer className="container-fluid">
+    <footer >
       <div id="footer" className="col-sm">
         <h1 id="footHead1">
           <img id="footImg" src="http://localhost:5000/static/logo.png" alt="Logo"/>
           Mindful Matters
         </h1>  
-        <nav>
-          <Link className="nav-link" id="navPrivacy" to="/Privacy">Privacy</Link>
-        </nav>
         <h2 id="footHead">AS FEATURED IN:</h2>
         <img id="hw" className="img-fluid" src="http://localhost:5000/static/hw.png" alt="health and wellbeing"></img>
         <img id="psych" className="img-fluid" src="http://localhost:5000/static/psych.png" alt="psychology today"></img>
