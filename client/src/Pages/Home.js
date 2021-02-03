66e695d69dc8678c96c4c1670e3876a6366bcff5
Jam.zD <jzdavis1212@gmail.com>
2021-02-03 22:46:23 +0000

    move headers and p elements

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -10,9 +10,11 @@ const Home = () => {
         <div id="contain">
             <div className="row">
                 <div className="col">
-                    <div id="pBox">
+                    <div className="topBox">
                        <h1 id="headHome">Be Kind to Yourself</h1>
-                       <p id="homeP">
+                    </div>
+                    <div id="pBox">
+                        <p id="homeP">
                            It all starts with just a few minutes a day to begin reducing stress and building
                            mental resilience. Mindfulness really is for everyone! You can try one of our mindfulness videos 
                            and start learning how to incorporate mindfulness into your life. 
@@ -20,14 +22,14 @@ const Home = () => {
                        <nav  id="navLink">
                           <Link id="Link"  to="/VideoPage">Start Your Journey with Mindful Matters</Link>
                         </nav>
-                    </div>
+                  </div>
                     <div id="vidContainer">
                        <div className="embed-responsive embed-responsive" id="videoPlayer">
                           <ReactPlayer url="https://www.youtube.com/watch?v=APX33ncDER0"/>  
                        </div>   
                   </div>  
                   <hr className="hrStyle" />
-                  <h2 id="homeStories">
+                  <h2 id="h2Stories">
                         Mindful Matters Stories 
                   </h2>
                </div>
--- client/src/Pages/VideoPage.js
+++ client/src/Pages/VideoPage.js
@@ -4,12 +4,10 @@ import './VideoPage.css';
 const VideoPage = () => {
   return (
     <div id="textContainer" >
-      <div id="textBackground" className="col">
+      <div className="topBox" >
         <h1 id="videoHead">Stop for a moment...</h1>
-        <p id="videoP">
-          Mindfulness can reduce stress and relax the mind.
-          Below is a carefully curated selection of videos you can watch now! 
-        </p>
+      </div>
+      <div>
         <form>
           <h3 id="formHead">How are you feeling today?</h3>
           <input className="hide" type="checkbox" title="Happy" name="happy" id="happy" />
@@ -19,6 +17,10 @@ const VideoPage = () => {
           <input className="hide" type="checkbox" title="Sad" name="sad" id="sad" />
           <label for="sad" className="option-sad">Sad</label>
         </form>
+        <p id="videoP">
+          Mindfulness can reduce stress and relax the mind.
+          Below is a carefully curated selection of videos you can watch now.
+        </p>
       </div>
    </div>
   ); 
